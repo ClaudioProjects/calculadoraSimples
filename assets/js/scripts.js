945b19c1d282edbb5b805a6b91543205cc462d47
Claudio Junior Olimpio <claudiojr2002@hotmail.com>
2022-01-25 09:36:42 -0300

    Mundaça de factory function para function constructor e correção de bug

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,79 +1,64 @@
-function criaCalculadora () {
-    return {
-        display: document.querySelector('.visor'),
+function Calculadora() {
+    const display = document.querySelector('.display');
 
-        inicia() {
-            this.clicaBotoes();
-            this.keyPressDgt();
-        },
+    this.inicia = () => {
+        clickBtn();
+        keyPressDgt();
+    }; 
+    
+    clickBtn = () => {
+        document.addEventListener('click', function(e) {
+            const element = e.target;
+            if (element.classList.contains('btnNum')) {
+                display.innerText += element.innerText;
+                display.focus()
+            };
+            if (element.classList.contains('btnClear')) {
+                clearDisplay();
+                display.focus()
+            };
+            if (element.classList.contains('btnDel')) {
+                delLastDgt();
+                display.focus()
+            };
+            if (element.classList.contains('btnEq')) {
+                finishCalc();
+                display.focus()
+            };
 
-        clicaBotoes() {
-            document.addEventListener('click', function(e) {
-                const element = e.target
-                if (element.classList.contains('btnNum')) {
-                    this.btnForDisplay(element.innerText);
-                };
-                if (element.classList.contains('btnClear')) {
-                    this.clearDisplay();
-                };
-                if (element.classList.contains('btnDel')) {
-                    this.delLastDigit();
-                };
-                if (element.classList.contains('btnEq')) {
-                    this.realizaconta();
-                };
-            }.bind(this));
-        },
+        });
+    };
+    const clearDisplay = () => {
+        display.innerText = '';
+    };
+    const delLastDgt = () => {
+        display.innerText = display.innerText.slice(0, -1);
+    };
+    const finishCalc = () => {
+        try {
+            let conta = display.innerText
+            conta = eval(conta)
+            display.innerText = conta;
+            if (isNaN(conta)) {
+                display.innerText = 'Invalid result'
+            }
+        }catch(error) {
+                display.innerText = 'Invalid result'
+            }
+    };
 
-        btnForDisplay(value) {
-            this.display.innerText += value;
-        },
-
-        clearDisplay() {
-            this.display.innerText = ' ';
-        },
-
-        delLastDigit() {
-            this.display.innerText = this.display.innerText.slice(0, -1);
-        },
-
-        keyPressDgt() {
+    const keyPressDgt = () => {
             document.addEventListener('keyup' , e => {
-                const num = ['1', '2', '3','4', '5', '6','7', '8', '9', '+', '-', '(',')', '*', '/'] 
-                for (let tecla of num) {
-                    if (e.key === tecla){
-                        this.display.innerText += e.key;
-                    };
-                };
-                if (e.key === 'Enter'){
-                    this.realizaconta();
-                };
-                if (e.key === "Backspace"){
-                    this.delLastDigit();
-                };
-                if (e.key === "Delete"){
-                    this.clearDisplay();
-                };
-            })
-        },
-
-        realizaconta() {
-            let conta = this.display.innerText;
-            try {
-                conta = eval(conta)
-                this.display.innerText = conta
-
-                if (isNaN(conta)) {
-                    this.display.innerText = 'Conta invalida'
-                    return
-                }
-            } catch(e) {
-                this.display.innerText = 'Conta invalida';
-                return
-                }     
-        },
-    }
+            const num = ['1', '2', '3','4', '5', '6','7', '8', '9', '+', '-', '(',')', '*', '/'] 
+            for (let tecla of num) {
+                if (e.key === tecla) display.innerText += e.key;
+            };
+            if (e.key === 'Enter') finishCalc();
+            if (e.key === "Backspace") delLastDgt();
+            if (e.key === "Delete") clearDisplay();
+        })
+    };
 };
 
-const calculadora = criaCalculadora();
-calculadora.inicia();
+const init = new Calculadora()
+init.inicia()
